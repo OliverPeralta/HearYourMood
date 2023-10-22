8ea2d449e018c5ffb675308388256c61226d775c
carboitel <boitelc@ufl.edu>
2023-10-22 10:28:53 -0400

    linked more buttons

--- src/app/second-mood/second-mood.component.ts
+++ src/app/second-mood/second-mood.component.ts
@@ -33,6 +33,18 @@ export class SecondMoodComponent implements OnInit {
       this.feelingImageSrc = 'https://i.pinimg.com/originals/61/f1/24/61f12406b4f0f01e4354e182a0533b36.gif';
     } else if (this.selectedFeeling == 'studious'){
       this.feelingImageSrc = 'https://media.tenor.com/8plhmX7JP9YAAAAC/study.gif';
+    } else if (this.selectedFeeling === 'relaxed') {
+      this.feelingImageSrc = 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdnA3Z3lwNWo5cHJhNmVpcWtmd2ZlNndhdDA3cXg0aTl1NHQ3OG1obSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/s1qPesR3kciW9qj9Cw/giphy.gif';
+    } else if (this.selectedFeeling === 'celebratory') {
+      this.feelingImageSrc = 'https://media.tenor.com/cxTcj3HA0CsAAAAd/mochi-peach.gif';
+    } else if (this.selectedFeeling === 'anxious') {
+      this.feelingImageSrc = 'https://images.squarespace-cdn.com/content/v1/5c92caa4a09a7e3ffc9e2f29/1556466261526-9V1UTXCG6QW8DH4E1CIM/sunday+4.gif';
+    } else if (this.selectedFeeling === 'romantic') {
+      this.feelingImageSrc = 'https://media.tenor.com/ZlndAzfwx80AAAAi/kiss-bubu-dudu.gif';
+    } else if (this.selectedFeeling === 'bored') {
+      this.feelingImageSrc = 'https://media.tenor.com/9DjJ4jUWIBoAAAAi/chabo-days-deer.gif';
+    } else if (this.selectedFeeling === 'unsure') {
+      this.feelingImageSrc = 'https://media.tenor.com/FwNcjKpHAvcAAAAi/shrug-molang.gif';
     }
 
     // Update the h1 text based on the selected feeling
